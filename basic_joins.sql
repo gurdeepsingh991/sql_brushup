0ffdcef23a21154d1c62a1f334bfdd5e085bd444
Gurdeep Singh <thisisgurdeep@gmail.com>
2025-10-14 16:43:02 +0100

    SQL joins brushup

---
+++ basic_joins.sql
@@ -0,0 +1,70 @@
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | id            | int     |
+-- | name          | varchar |
+-- +---------------+---------+
+-- id is the primary key (column with unique values) for this table.
+-- Each row of this table contains the id and the name of an employee in a company.
+ 
+
+-- Table: EmployeeUNI
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | id            | int     |
+-- | unique_id     | int     |
+-- +---------------+---------+
+-- (id, unique_id) is the primary key (combination of columns with unique values) for this table.
+-- Each row of this table contains the id and the corresponding unique id of an employee in the company.
+ 
+
+-- Write a solution to show the unique ID of each user, If a user does not have a unique ID replace just show null.
+
+-- Return the result table in any order.
+
+-- The result format is in the following example.
+-- SOlution
+select unique_id, name from Employees e left join EmployeeUNI eu on e.id=eu.id
+
+-- Q2
+-- +-------------+-------+
+-- | Column Name | Type  |
+-- +-------------+-------+
+-- | sale_id     | int   |
+-- | product_id  | int   |
+-- | year        | int   |
+-- | quantity    | int   |
+-- | price       | int   |
+-- +-------------+-------+
+-- (sale_id, year) is the primary key (combination of columns with unique values) of this table.
+-- product_id is a foreign key (reference column) to Product table.
+-- Each row of this table shows a sale on the product product_id in a certain year.
+-- Note that the price is per unit.
+ 
+
+-- Table: Product
+
+-- +--------------+---------+
+-- | Column Name  | Type    |
+-- +--------------+---------+
+-- | product_id   | int     |
+-- | product_name | varchar |
+-- +--------------+---------+
+-- product_id is the primary key (column with unique values) of this table.
+-- Each row of this table indicates the product name of each product.
+ 
+
+-- Write a solution to report the product_name, year, and price for each sale_id in the Sales table.
+
+-- Return the resulting table in any order.
+
+
+SELECT
+    p.product_name,
+    s.year,
+    s.price
+FROM Sales s
+left join Product p
+on s.product_id = p.product_id
