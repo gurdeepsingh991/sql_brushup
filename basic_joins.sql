1a61dd96033e3ec35944a619e2c964cc858f1bbd
Gurdeep Singh <thisisgurdeep@gmail.com>
2025-10-16 15:39:59 +0100

    continue sql join

--- basic_joins.sql
+++ basic_joins.sql
@@ -68,3 +68,141 @@ SELECT
 FROM Sales s
 left join Product p
 on s.product_id = p.product_id
+
+
+
+-- Q 3:
+-- Table: Visits
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | visit_id    | int     |
+-- | customer_id | int     |
+-- +-------------+---------+
+-- visit_id is the column with unique values for this table.
+-- This table contains information about the customers who visited the mall.
+ 
+
+-- Table: Transactions
+
+-- +----------------+---------+
+-- | Column Name    | Type    |
+-- +----------------+---------+
+-- | transaction_id | int     |
+-- | visit_id       | int     |
+-- | amount         | int     |
+-- +----------------+---------+
+-- transaction_id is column with unique values for this table.
+-- This table contains information about the transactions made during the visit_id.
+ 
+
+-- Write a solution to find the IDs of the users who visited without making any transactions and the number of times they made these types of visits.
+
+-- Return the result table sorted in any order.
+
+-- Solution
+
+select customer_id, count(customer_id) as count_no_trans from Visits v left outer join Transactions t on v.visit_id = t.visit_id where v.visit_id not in (select distinct visit_id from Transactions) group by v.customer_id;
+
+--Optimal solution
+SELECT
+    customer_id,
+    COUNT(customer_id) AS count_no_trans
+FROM Visits
+WHERE visit_id NOT IN (
+    SELECT visit_id
+    FROM Transactions
+)
+GROUP BY customer_id
+
+-- Q4
+-- Table: Weather
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | id            | int     |
+-- | recordDate    | date    |
+-- | temperature   | int     |
+-- +---------------+---------+
+-- id is the column with unique values for this table.
+-- There are no different rows with the same recordDate.
+-- This table contains information about the temperature on a certain day.
+ 
+
+-- Write a solution to find all dates' id with higher temperatures compared to its previous dates (yesterday).
+
+-- Return the result table in any order.
+
+select w.id from Weather w join Weather p on w.recordDate = DATE_ADD(p.recordDate, Interval 1 DAY) where w.temperature > p.temperature
+
+-- Q5
+-- +----------------+---------+
+-- | Column Name    | Type    |
+-- +----------------+---------+
+-- | machine_id     | int     |
+-- | process_id     | int     |
+-- | activity_type  | enum    |
+-- | timestamp      | float   |
+-- +----------------+---------+
+-- The table shows the user activities for a factory website.
+-- (machine_id, process_id, activity_type) is the primary key (combination of columns with unique values) of this table.
+-- machine_id is the ID of a machine.
+-- process_id is the ID of a process running on the machine with ID machine_id.
+-- activity_type is an ENUM (category) of type ('start', 'end').
+-- timestamp is a float representing the current time in seconds.
+-- 'start' means the machine starts the process at the given timestamp and 'end' means the machine ends the process at the given timestamp.
+-- The 'start' timestamp will always be before the 'end' timestamp for every (machine_id, process_id) pair.
+-- It is guaranteed that each (machine_id, process_id) pair has a 'start' and 'end' timestamp.
+ 
+
+-- There is a factory website that has several machines each running the same number of processes. Write a solution to find the average time each machine takes to complete a process.
+
+-- The time to complete a process is the 'end' timestamp minus the 'start' timestamp. The average time is calculated by the total time to complete every process on the machine divided by the number of processes that were run.
+
+-- The resulting table should have the machine_id along with the average time as processing_time, which should be rounded to 3 decimal places.
+
+-- Return the result table in any order.
+
+select a2.machine_id, ROUND((sum(a2.timestamp - a.timestamp))/(count(a2.machine_id)),3) as processing_time 
+from Activity a join Activity a2 
+on a.machine_id = a2.machine_id and a.process_id = a2.process_id 
+where a.activity_type = 'start' and a2.activity_type = 'end' group by a2.machine_id
+
+-- Q6 
+
+-- Table: Employee
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | empId       | int     |
+-- | name        | varchar |
+-- | supervisor  | int     |
+-- | salary      | int     |
+-- +-------------+---------+
+-- empId is the column with unique values for this table.
+-- Each row of this table indicates the name and the ID of an employee in addition to their salary and the id of their manager.
+ 
+
+-- Table: Bonus
+
+-- +-------------+------+
+-- | Column Name | Type |
+-- +-------------+------+
+-- | empId       | int  |
+-- | bonus       | int  |
+-- +-------------+------+
+-- empId is the column of unique values for this table.
+-- empId is a foreign key (reference column) to empId from the Employee table.
+-- Each row of this table contains the id of an employee and their respective bonus.
+ 
+
+-- Write a solution to report the name and bonus amount of each employee with a bonus less than 1000.
+
+-- Return the result table in any order.
+
+select name, bonus from employee e left join bonus b 
+on e.empId = b.empId 
+where b.bonus<1000 or bonus is null
